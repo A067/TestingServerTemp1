39352851e11c845cceb8be79033f7a4301e8ead7
Syed Ahmed <sahmed@flyht.com>
2018-10-06 18:31:11 -0600

    Commit to test node in Jenkins

--- HelloWorld/src/test/java/com/flyht/alpha/tests/TestLoginPage.java
+++ HelloWorld/src/test/java/com/flyht/alpha/tests/TestLoginPage.java
@@ -4,7 +4,7 @@ public class TestLoginPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hi TestLoginPage!");
+		System.out.println("Hello TestLoginPage!");
 	}
 
 }
